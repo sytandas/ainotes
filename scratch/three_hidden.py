9d4f7c086da90714f7ed7268d42ce6830c60339c
sayantan3 <sayantand975@gmail.com>
2023-06-14 18:37:43 +0530

    softmax-rocket

--- scratch/three_hidden.py
+++ scratch/three_hidden.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 np.random.seed(0)
 
@@ -8,29 +7,21 @@ X = [[1, 2, 3, 2.5],
      [-1.5, 2.7, 3.3, -0.8]]
 
 # dataset
-def create_data(points, classes):
-    X = np.zeros((points*classes, 2))
-    y = np.zeros(points*classes, dtype='uint8')
+def spiral_data(samples, classes):
+    X = np.zeros((samples*classes, 2))
+    y = np.zeros(samples*classes, dtype='uint8')
     for class_number in range(classes):
-        ix = range(points*class_number, points*(class_number+1))
-        r = np.linspace(0.0, 1, points) # radius
-        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        ix = range(samples*class_number, samples*(class_number+1))
+        r = np.linspace(0.0, 1, samples)
+        t = np.linspace(class_number*4, (class_number+1)*4, samples) + np.random.randn(samples)*0.2
         X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
         y[ix] = class_number
-    return X, y
-
-X, y = create_data(100, 3)
-plt.scatter(X[:,0],X[:,1])
-plt.savefig('plot.jpg', format='jpg')
-
-plt.scatter(X[:,0], X[:,1], c=y, cmap='brg')
-plt.savefig('plot1.jpg', format='jpg')
-
+    return X, y 
 # layer
 class Layer_dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros(1, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
@@ -39,8 +30,24 @@ class activation_relu:
     def forward(self, inputs):
         self.outputs = np.maximum(0, inputs)
 
-layer1 = Layer_dense(4, 5)
-activation1 = activation_relu()
-layer1.forward(X)
-activation1.forward(layer1)
+class activation_solftmax:
+    def forward(self, inputs):
+        exp_value = np.exp(inputs - np.max(inputs, axis=1, keepdims=True)) # input - max(input) -> for avoiding overflow
+        norm_value = np.sum(exp_value, axis=1, keepdims=True)
+        self.outuput = exp_value / norm_value
 
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 =Layer_dense(2,3)
+activation1 = activation_relu()
+
+dense2 = Layer_dense(3, 3)
+activation2 = activation_solftmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.outputs)
+activation2.forward(dense2.output)
+
+print(activation2.outuput[:5])
