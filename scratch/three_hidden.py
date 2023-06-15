6d7229735da5b835651516dc80021ccb69dd31fa
sayantan3 <sayantand975@gmail.com>
2023-06-15 21:57:04 +0530

    update

--- scratch/three_hidden.py
+++ scratch/three_hidden.py
@@ -36,6 +36,26 @@ class activation_solftmax:
         norm_value = np.sum(exp_value, axis=1, keepdims=True)
         self.outuput = exp_value / norm_value
 
+class loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_losses = np.mean(sample_losses)
+        return data_losses
+    
+class loss_CategoricalCrossEntropy(loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7) # clippping value near to infinity avoid log(0) for y->prediction
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape()) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis = 1)
+        
+        negative_log_likelihood = -np.log(correct_confidences)
+        return negative_log_likelihood
+
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 =layer_dense(2,3)
@@ -51,3 +71,8 @@ dense2.forward(activation1.outputs)
 activation2.forward(dense2.output)
 
 print(activation2.outuput[:5])
+
+loss_function = loss_CategoricalCrossEntropy()
+loss = loss_function.calculate(activation2.outuput, y)
+
+print(loss)
