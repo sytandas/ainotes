8cdf16c9a2f1a4aa7208d5eb8b69adc6943075d4
sytandas <sytandas@gmail.com>
2024-09-02 20:20:57 +0530

    im

--- mlperf/inf.py
+++ mlperf/inf.py
@@ -1,6 +1,6 @@
 # how to do mlperf on tinygrad stable diffusion!!!
 # findings: should be written from scratch after understanding the codebase and goals.
-# tinygrad -> examples -> mlperf
-# trying to understand how to do inference on tinygrad! from the old commit possibly. 
+# old commit are unclear  > need of research 
 
-# t1: choose dataset which to use for sd, trail run on es@dl, early morning 2nd Sep. 
+# t1: choose dataset which to use for sd, trail run on es@dl 
+
