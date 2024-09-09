9bacf60080870d6f90d4e0b256a80fa5dc1db019
sytandas <sytandas@gmail.com>
2024-09-09 22:14:41 +0530

    input

--- mlperf/inf.py
+++ mlperf/inf.py
@@ -1,7 +1,11 @@
 # how to do mlperf on tinygrad stable diffusion!!!
 # findings: should be written from scratch after understanding the codebase and goals.
 # old commit are unclear  > need of research 
-
-# t1: choose dataset which to use for sd, trail run on es@dl 
-# t2: review and reread, analyze is the approach is any value or not
-
+# 
+# something about scheduling and handling the code from kernal level to output
+# the type of document he came of by reverse engineering that has to be learn for deep and practical understanding
+# there are still complex ideas in tandom with lazydata, realize 
+# realize is what data should be allocated memory in the GPU contains 6 lazydata - MUL1, COPY2, EXT8, CONST7, MUL10 so on need to check
+# realizes using directly to generate schedule items
+# allbufs just to use stop creating same tree again
+# lazy buffer is casting data to chneel to process at my language and iterate through _scheduler_lb
