5cfb0bfee6a6b55db6a9305834947093cf55d2b8
sytandas <sytandas@gmail.com>
2025-05-20 20:41:04 +0530

    work on

--- cifar-10/hlb-cifar.py
+++ cifar-10/hlb-cifar.py
@@ -1,3 +1,7 @@
+"""
+hlb_cifar10 using torch backend for tinygradA
+implementation of https://github.com/tysam-code/hlb-CIFAR10/blob/main/main.py
+"""
 import torch
 import torch.nn as nn
 import torch.optim as optim 
