538b942485ad38ea2ea51cc0b274580a9c3609a5
sytandas <sytandas@gmail.com>
2025-05-21 20:51:32 +0530

    minor

--- cifar-10/hlb-cifar.py
+++ cifar-10/hlb-cifar.py
@@ -51,41 +51,7 @@ hyp = {
         'data_location': 'data.pt',
     }
 }
-
-
-hyp = {
-    'opt': {
-        'bias_lr':        1.525 * bias_scaler/512, # TODO: Is there maybe a better way to express the bias and batchnorm scaling? :'))))
-        'non_bias_lr':    1.525 / 512,
-        'bias_decay':     6.687e-4 * batchsize/bias_scaler,
-        'non_bias_decay': 6.687e-4 * batchsize,
-        'scaling_factor': 1./9,
-        'percent_start': .23,
-        'loss_scale_scaler': 1./32, # * Regularizer inside the loss summing (range: ~1/512 - 16+). FP8 should help with this somewhat too, whenever it comes out. :)
-    },
-    'net': {
-        'whitening': {
-            'kernel_size': 2,
-            'num_examples': 50000,
-        },
-        'batch_norm_momentum': .4, # * Don't forget momentum is 1 - momentum here (due to a quirk in the original paper... >:( )
-        'cutmix_size': 3,
-        'cutmix_epochs': 6,
-        'pad_amount': 2,
-        'base_depth': 64 ## This should be a factor of 8 in some way to stay tensor core friendly
-    },
-    'misc': {
-        'ema': {
-            'epochs': 10, # Slight bug in that this counts only full epochs and then additionally runs the EMA for any fractional epochs at the end too
-            'decay_base': .95,
-            'decay_pow': 3.,
-            'every_n_steps': 5,
-        },
-        'train_epochs': 12.1,
-        'device': 'cuda',
-        'data_location': 'data.pt',
-    }
-}# data loader
+# data loader
 if not os.path.exists(hyp['misc']['data_location']):
 
         transform = transforms.Compose([
